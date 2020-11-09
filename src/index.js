d5cb8a2b55fc44329d196ae4f60438be19943163
rakuda3dev <shoot11834@gmail.com>
2020-11-10 00:15:59 +0900

    3_2_コンポーネントの組み合わせ

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-    <App title="App" message="This is App component" />,
+    <App />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
