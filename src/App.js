4fdcbffa556ecc34a00a7c7659db9c226dac3951
rakuda3dev <shoot11834@gmail.com>
2021-01-02 23:45:44 +0900

    4_1_Reduxを使ってみる

--- src/App.js
+++ src/App.js
@@ -1,59 +1,83 @@
 import React, { Component} from 'react';
-import Rect from './Rect';
+import {connect} from 'react-redux';
 import './App.css';
 import { identifier } from '@babel/types';
 
-let theme = {
-  light:{
-    backgroundColor:"#eef",
-    color:"#006",
-    padding:"10px",
-  },
-  dark:{
-    backgroundColor:"#006",
-    color:"eef",
-    padding:"10px",
-  }
-};
-
-const ThemeContext = React.createContext(theme.light); // light or dark で見た目が変わる
-
-class App extends Component{
-
-  static contextType = ThemeContext;
-
-  render(){
-    return (
-      <div style={this.context}>
-        <Title value="Content Page"/>
-        <Message value="This is Content sample"/>
-        <Message value="This is Theme sample" />
-      </div>
-    );
-  }
+// mapping of state
+function mappingState(state){
+  return state;
 }
 
-class Title extends Component{
-  static contextType = ThemeContext;
+//App component
+class App extends Component{
+
+  constructor(props){
+    super(props);
+  }
 
   render(){
     return (
       <div>
-        <h2 style={this.context}>{this.props.value}</h2>
+        <h1>Redux</h1>
+        <Message />
+        <Button />
       </div>
     );
   }
 }
 
+// connect of state
+App = connect()(App);
+
+// component of message
 class Message extends Component {
 
-  static contextType = ThemeContext;
+  style = {
+    fontSize:"20pt",
+    padding:"20px 5px"
+  }
 
   render(){
     return (
-      <p style={this.context}>{this.props.value}</p>
+      <p style={this.style}>{this.props.counter}</p>
     );
   }
 }
+
+// connect of store
+Message = connect(mappingState)(Message);
+
+// component of button view
+class Button extends Component{
+  style = {
+    fontSize:"16pt",
+    padding:"5px 10px"
+  }
+
+  constructor(props){
+    super(props);
+    this.doAction = this.doAction.bind(this);
+  }
+
+  // excute dispatch
+  doAction(e){
+    if (e.shiftKey){
+      this.props.dispatch({type:"DECREMENT"});
+    } else{
+      this.props.dispatch({type:'INCREMENT'});
+    }
+  }
+
+  render(){
+    return (
+      <button style={this.style} onClick ={this.doAction}>
+        click
+      </button>
+    );
+  }
+}
+
+//connect of state
+Button = connect()(Button);
 
 export default App;
