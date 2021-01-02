b319a66103f2d6e560b46aa66ebbba53dfbc8101
rakuda3dev <shoot11834@gmail.com>
2021-01-03 00:55:28 +0900

    4_2_メモアプリ:indexとapp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component} from 'react';
 import {connect} from 'react-redux';
 import './App.css';
+import Memo from './memo/Memo';
+import AddForm from './memo/AddForm';
+import FindForm from '/memo/FindForm';
+import DelForm from './memo/DelForm';
 import { identifier } from '@babel/types';
-
-// mapping of state
-function mappingState(state){
-  return state;
-}
 
 //App component
 class App extends Component{
+
+  td = {
+    width:"250px"
+  }
 
   constructor(props){
     super(props);
@@ -18,66 +21,16 @@ class App extends Component{
   render(){
     return (
       <div>
-        <h1>Redux</h1>
-        <Message />
-        <Button />
+        <h1>Memo</h1>
+        <AddForm />
+        <hr />
+        <table><tbody><tr>
+          <td style={this.td}><FindForm /></td>
+          <td style={this.td}><DelForm /></td>  
+          </tr></tbody></table>
       </div>
     );
   }
 }
 
-// connect of state
-App = connect()(App);
-
-// component of message
-class Message extends Component {
-
-  style = {
-    fontSize:"20pt",
-    padding:"20px 5px"
-  }
-
-  render(){
-    return (
-      <p style={this.style}>{this.props.counter}</p>
-    );
-  }
-}
-
-// connect of store
-Message = connect(mappingState)(Message);
-
-// component of button view
-class Button extends Component{
-  style = {
-    fontSize:"16pt",
-    padding:"5px 10px"
-  }
-
-  constructor(props){
-    super(props);
-    this.doAction = this.doAction.bind(this);
-  }
-
-  // excute dispatch
-  doAction(e){
-    if (e.shiftKey){
-      this.props.dispatch({type:"DECREMENT"});
-    } else{
-      this.props.dispatch({type:'INCREMENT'});
-    }
-  }
-
-  render(){
-    return (
-      <button style={this.style} onClick ={this.doAction}>
-        click
-      </button>
-    );
-  }
-}
-
-//connect of state
-Button = connect()(Button);
-
-export default App;
+export default connect()(App);
