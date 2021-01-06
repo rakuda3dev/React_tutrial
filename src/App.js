a8d37cf891955c6d9ba28c6fa54873bda2b62007
rakuda3dev <shoot11834@gmail.com>
2021-01-06 23:29:35 +0900

    4_3_カウントアプリの永続化:app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React, { Component} from 'react';
 import {connect} from 'react-redux';
 import './App.css';
-import Memo from './memo/Memo';
-import AddForm from './memo/AddForm';
-import FindForm from './memo/FindForm';
-import DelForm from './memo/DelForm';
-import { identifier } from '@babel/types';
+import { identifier, thisExpression } from '@babel/types';
 
 //App component
 class App extends Component{
-
-  td = {
-    width:"250px"
-  }
 
   constructor(props){
     super(props);
@@ -21,17 +13,68 @@ class App extends Component{
   render(){
     return (
       <div>
-        <h1>Memo</h1>
-        <AddForm />
-        <hr />
-        <table><tbody><tr>
-          <td style={this.td}><FindForm /></td>
-          <td style={this.td}><DelForm /></td>  
-          </tr></tbody></table>
-        <Memo />
+        <h1>Redux</h1>
+        <Message />
+        <Button />
       </div>
     );
   }
 }
+// connect store
+App = connect()(App);
+
+// Message component
+class Message extends Component{
+  style = {
+    fontSize:"20pt",
+    padding:"20px 5px"
+  }
+
+  render(){
+    return (
+      <p style={this.style}>
+        {this.props.message} : {this.props.counter}
+      </p>
+    );
+  }
+}
+
+// connect store
+Message = connect((state)=>state)(Message);
+
+// button component
+class Button extends Component{
+  style= {
+    fontSize:"16pt",
+    padding:"5px 10px"
+  }
+
+  constructor(props){
+    super(props);
+    this.doAction = this.doAction.bind(this);
+  }
+
+  // dispatch when click dispatch
+  doAction(e){
+    if(e.shiftKey){
+      this.props.dispatch({type:'DECREMENT' });
+    } else if (e.ctrlKey){
+      this.props.dispatch({type:'RESET'});
+    } else {
+      this.props.dispatch({type:'INCREMENT'});
+    }
+  }
+
+  render(){
+    return(
+      <button style={this.style} onClick={this.doAction}>
+        click
+      </button>
+    );
+  }
+}
+
+// connect store
+Button = connect()(Button);
 
 export default connect()(App);
